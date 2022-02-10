d98f69b195dcf0f21c91e2d6693acb384bceff6b
Jordan Pearl <masterjp9@gmail.com>
2022-02-09 19:11:51 -0500

    Turrets can fire at player

--- Assets/Scripts/Enemies/EnemyTurret.cs
+++ Assets/Scripts/Enemies/EnemyTurret.cs
@@ -2,19 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class EnemyTurret : Destructible
 {
+    public float range = 15f, fireDelay = 2.5f;
+    bool bTargetInRange, bFire;
+
+    #region Components
+    [Header("Componenets")]
+    [SerializeField] ParticleSystem[] Blasters;
+    [SerializeField] Transform Turret, BarrelPivot;
     Transform Target;
     HealthStatus healthStatus;
-    public float range = 15f;
-    bool bTargetInRange;
+    AudioSource audioSource;
+    #endregion
 
-    [SerializeField] Transform Turret, BarrelPivot;
-    [SerializeField] GameObject explosion, smoke, electricity;
+    #region Particle Effects
+    [Header("Particle Effect Prefabs")]
+    [SerializeField] GameObject explosion;
+    [SerializeField] GameObject smoke;
+    [SerializeField] GameObject electricity;
+    #endregion
 
     void Awake()
     {
         Target = GameObject.Find("Player").transform;
+        audioSource = GetComponent<AudioSource>();
         healthStatus = HealthStatus.FULL;
     }
     
@@ -33,15 +46,27 @@ public class EnemyTurret : Destructible
         //TestAim();
     }
 
+    #region Targeting Methods
+
     void UpdateTarget()
     {
-        if(Target != null)
+        if(Target == null)
         {
-            float distanceToTarget = Vector3.Distance(transform.position, Target.position);
-            bTargetInRange = distanceToTarget <= range;
-        }
-        else
             bTargetInRange = false;
+            bFire = false;
+            return;
+        }
+
+        float distanceToTarget = Vector3.Distance(transform.position, Target.position);
+        bTargetInRange = distanceToTarget <= range;
+
+        Vector3 targetDirection = (Target.position - BarrelPivot.position);
+        float angle = Vector3.Angle(targetDirection, BarrelPivot.forward);
+        
+        if(angle < 10)
+            TargetInSight();
+        else
+            bFire = false;
     }
 
     // Works with Grounded Turret
@@ -75,6 +100,42 @@ public class EnemyTurret : Destructible
         Turret.rotation = Quaternion.Euler(SmoothTurretRotation);
         BarrelPivot.rotation = Quaternion.Euler(BarrelRotation);
     }
+
+    void TargetInSight()
+    {
+        //float distanceFromTarget = (Target.position - BarrelPivot.position).magnitude;
+        
+        //Debug.Log("Angle from " + gameObject.name + " = " + angle);
+        if(!bTargetInRange || bFire)
+            return;
+
+        bFire = true;
+        InvokeRepeating("Fire", 0, fireDelay);
+    }
+
+    void Fire()
+    {
+        if(!bFire)
+        {
+            CancelInvoke("Fire");
+            return;
+        }
+
+        foreach(var blaster in Blasters)
+        {
+            blaster.Play();
+
+            if(Physics.Raycast(blaster.transform.position, blaster.transform.forward, out RaycastHit hit))
+            {
+                if(hit.collider.gameObject.TryGetComponent<PlayerStats>(out PlayerStats pStats))
+                    pStats.TakeDamage(5, hit.normal);
+            }
+        }
+        audioSource.Play();
+    }
+    #endregion
+
+    #region Damage Methods
 
     public override void TakeDamage(int damage)
     {
@@ -121,12 +182,12 @@ public class EnemyTurret : Destructible
         exp.GetComponent<Explosion>().Init(2, 2, 2, StatusEffect.BURN, 2f);
         Destroy(gameObject,0.1f);
     }
+    #endregion
 
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, range);
-        Gizmos.DrawLine(Turret.position, Turret.position + Turret.forward);
         Gizmos.DrawLine(Turret.position, Turret.position + Turret.up);
         Gizmos.DrawLine(BarrelPivot.position, BarrelPivot.position + BarrelPivot.forward);
     }
