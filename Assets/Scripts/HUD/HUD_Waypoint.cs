1ef0b6bbf01264308cfc97bf7f87248e3c33693d
Jordan Pearl <masterjp9@gmail.com>
2022-02-19 17:35:29 -0500

    HUD waypoint is now properly bound to screen

--- Assets/Scripts/HUD/HUD_Waypoint.cs
+++ Assets/Scripts/HUD/HUD_Waypoint.cs
@@ -3,6 +3,8 @@ using UnityEngine.UI;
 
 public class HUD_Waypoint : MonoBehaviour
 {
+    [SerializeField] Transform player;
+
     [Header("Objectives")]
     public GameObject ObjectivePanel;
     public Text ObjectiveText;
@@ -10,7 +12,9 @@ public class HUD_Waypoint : MonoBehaviour
 
     [Header("Waypoint Variables")]
     public Image waypoint;
-    public bool bLocation;
+    public Text meterDistance;
+    public Vector3 waypointOffset;
+    [HideInInspector] public bool bLocation;
     
     void Update()
     {
@@ -20,6 +24,7 @@ public class HUD_Waypoint : MonoBehaviour
         PositionWaypoint();
     }
 
+    // Credit to Omar Balfaqih yt, "Waypoint Marker | Unity"
     void PositionWaypoint()
     {
         float minX = waypoint.GetPixelAdjustedRect().width / 2;
@@ -28,11 +33,21 @@ public class HUD_Waypoint : MonoBehaviour
         float minY = waypoint.GetPixelAdjustedRect().height / 2;
         float maxY = Screen.height - minY;
 
-        Vector2 pos = Camera.main.WorldToScreenPoint(target);
+        Vector2 pos = Camera.main.WorldToScreenPoint(target + waypointOffset);
         pos.x = Mathf.Clamp(pos.x, minX, maxX);
         pos.y = Mathf.Clamp(pos.y, minY, maxY);
 
+        if(Vector3.Dot((target - player.position), player.forward) < 0)
+        {
+            //Target is behind player
+            if(pos.x < Screen.width / 2)
+                pos.x = maxX;
+            else
+                pos.x = minX;
+        }
+
         waypoint.transform.position = pos;
+        meterDistance.text = ((int)Vector3.Distance(target, player.position)).ToString() + "m";
     }
 
     public void SetTarget(Vector3 location)
@@ -46,5 +61,11 @@ public class HUD_Waypoint : MonoBehaviour
     {
         bLocation = false;
         waypoint.gameObject.SetActive(false);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(player.position, player.position + player.forward);
     }
 }
