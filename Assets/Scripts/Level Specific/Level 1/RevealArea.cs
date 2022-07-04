d5010e2b31acf3067c9077f16d451338b04957c2
Jordan Pearl <masterjp9@gmail.com>
2022-07-04 18:47:43 -0400

    Added FadeIn to objects in Level 1

--- Assets/Scripts/Level Specific/Level 1/RevealArea.cs
+++ Assets/Scripts/Level Specific/Level 1/RevealArea.cs
@@ -4,9 +4,45 @@ using UnityEngine;
 
 public class RevealArea : MonoBehaviour
 {
-    public void Reveal()
+    Material sMaterial;
+    List<Material> childrenMaterials = new List<Material>();
+
+    float dissolveAmount = 1f;
+    void Start()
     {
         foreach(Transform child in transform)
-            child.gameObject.SetActive(true);
+        {
+            var obj = child.gameObject;
+
+            Material[] materials = obj.GetComponent<MeshRenderer>().materials;
+            sMaterial = new Material(materials[0]);
+            materials[0] = sMaterial;
+            obj.GetComponent<MeshRenderer>().materials = materials;
+
+            sMaterial.SetFloat("_DissolveAmount", dissolveAmount);
+            sMaterial.SetFloat("_DissolveWidth", .05f);
+            childrenMaterials.Add(sMaterial);
+
+            obj.GetComponent<Collider>().enabled = false;
+        }
+    }
+    public void Reveal()
+    {
+        InvokeRepeating("FadeIn", 0, 0.01f);
+        foreach(Transform child in transform)
+        {
+            var obj = child.gameObject;
+            obj.GetComponent<Collider>().enabled = true;
+        }
+    }
+
+    void FadeIn()
+    {
+        dissolveAmount = Mathf.Clamp01(dissolveAmount - 0.01f);
+        foreach(var mat in childrenMaterials)
+            mat.SetFloat("_DissolveAmount", dissolveAmount);
+
+        if(dissolveAmount == 0f)
+            CancelInvoke("FadeIn");
     }
 }
